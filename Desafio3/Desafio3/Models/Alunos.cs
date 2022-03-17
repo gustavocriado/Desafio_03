ef3ffdc683e74d0624da0bb150fd7f5960716bb3
Gustavo Criado <gustavoliracriado30@gmail.com>
2022-03-16 21:17:54 -0300

    Desafio_Concluido

---
+++ Desafio3/Desafio3/Models/Alunos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3.Models
+{
+    public class Alunos
+    {
+        public string Nome { get; set; }
+        public double Nota { get; set; }
+    }
+}
---
+++ Desafio3/Desafio3/Program.cs
@@ -0,0 +1,37 @@
+﻿using Desafio3.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alunos> alunos = new List<Alunos>();
+            
+            Console.WriteLine("Digite a quantidade de alunos: ");
+            var quantidadeAlunos = Convert.ToInt32(Console.ReadLine());
+
+            if (quantidadeAlunos > 0) 
+            {
+                for (int i = 0; i < quantidadeAlunos; i++)
+                {
+                    Alunos aluno = new Alunos();
+
+                    Console.WriteLine("Digite o Nome do aluno: ");
+                    aluno.Nome = Console.ReadLine();
+                    Console.WriteLine("\nDigite o nota do aluno: ");
+                    aluno.Nota = Convert.ToDouble(Console.ReadLine());
+
+                    alunos.Add(aluno);
+                }
+            }
+
+            var maiorNota = alunos.Select(c => c.Nota).Max();
+            Console.WriteLine($"O Aluno com maior nota é: {alunos.Where(x => x.Nota == maiorNota).Select(x => x.Nome).FirstOrDefault()} \n");
+            Console.ReadKey();
+        }
+    }
+}
